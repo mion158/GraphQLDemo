f588704871ae54d2446aa786b51235799b4a931f
mion158 <111487792+mion158@users.noreply.github.com>
2022-10-12 21:57:59 +0100

    put exceptions

--- src/main/java/com/code/GraphQLDemo/resolver/Mutation.java
+++ src/main/java/com/code/GraphQLDemo/resolver/Mutation.java
@@ -1,5 +1,8 @@
 package com.code.GraphQLDemo.resolver;
 
+import java.util.Optional;
+
+import com.code.GraphQLDemo.exceptions.BookNotFoundException;
 import com.code.GraphQLDemo.model.*;
 import com.code.GraphQLDemo.repository.*;
 import com.coxautodev.graphql.tools.GraphQLMutationResolver;
@@ -12,6 +15,45 @@ public class Mutation implements GraphQLMutationResolver {
     public Mutation(AuthorRepository authorRepository, BookRepository bookRepository) {
         this.authorRepository = authorRepository;
         this.bookRepository = bookRepository;
+    }
+
+    public Author newAuthor(String firstName, String lastName) {
+        Author author = new Author();
+        author.setFirstName(firstName);
+        author.setLastName(lastName);
+
+        authorRepository.save(author);
+
+        return author;      
+    }
+
+    public Book newBook(String title, String isbn, Integer pageCount, Long authorId) {
+        Book book = new Book();
+        book.setAuthor(new Author(authorId));
+        book.setTitle(title);
+        book.setIsbn(isbn);
+        book.setPageCount(pageCount != null ? pageCount : 0);
+
+        bookRepository.save(book);
+        return book;
+    }
+
+    public boolean deleteBook(Long id) {
+        bookRepository.deleteById(id);
+        return true;
+    }
+
+    public Book updatePageCount (Integer pageCount, Long id) {
+        Optional<Book> optionalBook = bookRepository.findById(id);
+        if (optionalBook.isPresent()) {
+            Book book = optionalBook.get();
+            book.setPageCount(pageCount);
+            bookRepository.save(book);
+
+            return book;
+        }
+        throw new BookNotFoundException("the book was not found", id);
+    
     }
     
 }
