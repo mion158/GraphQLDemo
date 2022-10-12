a31a747adf0d04037ddfaeeea3ff83e1ce4835b9
mion158 <111487792+mion158@users.noreply.github.com>
2022-10-12 21:38:08 +0100

    added query class

---
+++ src/main/java/com/code/GraphQLDemo/resolver/Query.java
@@ -0,0 +1,32 @@
+package com.code.GraphQLDemo.resolver;
+
+import com.code.GraphQLDemo.model.*;
+import com.code.GraphQLDemo.repository.*;
+
+import com.coxautodev.graphql.tools.GraphQLQueryResolver;
+
+public class Query implements GraphQLQueryResolver{
+    private BookRepository bookRepository;
+    private AuthorRepository authorRepository;
+
+    public Query(AuthorRepository authorRepository, BookRepository bookRepository) {
+        this.authorRepository = authorRepository;
+        this.bookRepository = bookRepository;
+    }
+
+    public Iterable<Book> findAllBooks() {
+        return bookRepository.findAll();
+    }
+
+    public Iterable<Author> findAllAuthors() {
+        return authorRepository.findAll();
+    }
+
+    public long countBooks() {
+        return bookRepository.count();
+    }
+
+    public long countAuthors() {
+        return authorRepository.count();
+    }
+}
