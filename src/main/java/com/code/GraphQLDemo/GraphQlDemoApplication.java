827b1c858b16010da7d8a83fedf7467f61db4574
mion158 <111487792+mion158@users.noreply.github.com>
2022-10-13 20:16:19 +0100

    declared spring beans

--- src/main/java/com/code/GraphQLDemo/GraphQlDemoApplication.java
+++ src/main/java/com/code/GraphQLDemo/GraphQlDemoApplication.java
@@ -2,6 +2,13 @@ package com.code.GraphQLDemo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import com.code.GraphQLDemo.repository.AuthorRepository;
+import com.code.GraphQLDemo.repository.BookRepository;
+import com.code.GraphQLDemo.resolver.BookResolver;
+import com.code.GraphQLDemo.resolver.Mutation;
+import com.code.GraphQLDemo.resolver.Query;
 
 @SpringBootApplication
 public class GraphQlDemoApplication {
@@ -9,5 +16,21 @@ public class GraphQlDemoApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(GraphQlDemoApplication.class, args);
 	}
+
+	@Bean
+	public BookResolver authorResolver(AuthorRepository authorRepository) {
+		return new BookResolver(authorRepository);
+	}
+
+	@Bean
+	public Query query(AuthorRepository authorRepository, BookRepository bookRepository) {
+		return new Query(authorRepository, bookRepository);
+	}
+
+	@Bean
+	public Mutation mutation(AuthorRepository authorRepository, BookRepository bookRepository) {
+		return new Mutation(authorRepository, bookRepository);
+	}
+
 
 }
