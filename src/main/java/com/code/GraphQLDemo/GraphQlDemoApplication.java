e9b3e7c028290e635058a54c6d2e7b1cb5af8051
mion158 <111487792+mion158@users.noreply.github.com>
2022-10-13 20:23:06 +0100

    added some data

--- src/main/java/com/code/GraphQLDemo/GraphQlDemoApplication.java
+++ src/main/java/com/code/GraphQLDemo/GraphQlDemoApplication.java
@@ -1,9 +1,12 @@
 package com.code.GraphQLDemo;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 
+import com.code.GraphQLDemo.model.Author;
+import com.code.GraphQLDemo.model.Book;
 import com.code.GraphQLDemo.repository.AuthorRepository;
 import com.code.GraphQLDemo.repository.BookRepository;
 import com.code.GraphQLDemo.resolver.BookResolver;
@@ -32,5 +35,14 @@ public class GraphQlDemoApplication {
 		return new Mutation(authorRepository, bookRepository);
 	}
 
+	@Bean
+	public CommandLineRunner demo(AuthorRepository authorRepository, BookRepository bookRepository) {
+		return(args) -> {
+			Author author = new Author("Shinkai", "Makoto");
+			authorRepository.save(author);
+
+			bookRepository.save(new Book("Kotonoha no niwa", "ABC123456", 789, author));
+		};
+	}
 
 }
